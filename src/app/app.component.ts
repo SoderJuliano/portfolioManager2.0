a5e1cbe085b1a0c82b3a732754cdd395bb1295b9
SoderJuliano <julianosoder1989@gmail.com>
2023-12-17 22:36:02 -0300

    started developing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,12 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'portfoliomanager2.0';
+  myvar: string = "hi";
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.myvar = "Im a variable";
+  }
 }
+
